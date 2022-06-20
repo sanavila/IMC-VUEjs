2caeae63b191ecd41e160a6c05519f803973a6c9
sanavila <rute.leone@hotmail.com>
2022-06-20 19:56:27 -0300

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,50 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+   Peso: 
+   <input type="number" v-model="peso"/>
+   Altura: 
+   <input type="number" v-model="altura"/>
+   <p>{{resultado}}</p>
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data(){
+    return {
+      peso: 0.0,
+      altura: 0.0
+    }
+  },
+  computed: {
+    resultado () {
+      const IMC =  parseFloat(this.peso)  / (parseFloat(this.altura)*parseFloat(this.altura));
+      if (!this.peso) {
+        return "Preencha com um valor válido o peso"; 
+      } else if (!this.altura){
+        return "Preencha com um valor válido a altura";
+      } else if(IMC < 18.5) {
+        return "Abaixo do peso"
+      } else if (IMC < 25) {
+        return "Peso Normal"
+      } else if (IMC < 30 ) {
+        return "Sobrepeso"
+      } else if (IMC < 35) {
+        return "Obesidade grau 1"
+      } else if (IMC < 40) {
+        return "Obesidade grau 2"
+      } else if (IMC >= 40) {
+        return "Obesidade grau 3 ou Mórbida"
+      }
+      return "Digite os valores"
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
